843293162eba6c6c8c7c65a833e625a82b842e10
John Doe <johndoe@example.com>
2020-11-24 16:01:10 +0800

    ok

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\InspiringController;
 
 /*
 |--------------------------------------------------------------------------
@@ -28,3 +29,5 @@ Route::get('/about_us', function () {
     return view('about_us', ['name' => 'Laravel 6.0 範例']);
 
 });
+
+Route::get('/inspire', 'App\Http\Controllers\InspiringController@inspire');
